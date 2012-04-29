5d53c395e7a70351ecd9e065d6fffdc9df1525c8
Rohan Singh <rohan@washington.edu>
2012-04-29 17:32:00 +0200

    Added link-jars-for-osx.sh.
    In order to use Maven, we need to do some `ln` magic.

---
+++ link-jars-for-osx.sh
@@ -0,0 +1,2 @@
+ln -s /System/Library/Frameworks/JavaVM.framework/Classes/classes.jar /System/Library/Java/JavaVirtualMachines/1.6.0.jdk/Contents/Home/lib/rt.jar
+ln -s /System/Library/Java/JavaVirtualMachines/1.6.0.jdk/Contents/Classes/jsse.jar /System/Library/Java/JavaVirtualMachines/1.6.0.jdk/Contents/Home/lib/jsse.jar
